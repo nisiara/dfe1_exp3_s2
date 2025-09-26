91ccbaa98aa2ed39078bc5cd2337e69c32144de7
Nicolas Silva <nic.silvaa@duocuc.cl>
2025-09-26 00:09:59 -0300

    Create Header component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,27 @@
+import './Header.sass'
+import logo from '../../assets/images/logo.png'
+
+const Header = () => {
+  return (
+    <header>
+    <h1>
+      <span>next up games</span>
+      <img src={logo} alt="next up games" width="180" height="72" />
+    </h1>
+    
+    <nav>
+      <ul>
+        <li><a href="index.html" aria-current="page">Inicio</a></li>
+        <li><a href="#">Ofertas</a></li>
+        <li><a href="#">Consolas</a></li>
+        <li><a href="#">Accesorios</a></li>
+        <li><a href="#">Contacto</a></li>
+      </ul>
+    </nav>
+  </header>
+  )
+  
+  
+}
+
+export default Header;
